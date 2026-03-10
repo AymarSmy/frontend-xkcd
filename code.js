ae4f08aa793cee477e4f79dea34ba80f35f72919
Aymar Smyej <aymar.smyej@etu.minesparis.psl.eu>
2026-03-10 16:24:22 +0100

    Fetch Issue

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
 const XKCD = "https://xkcd.now.sh/?comic="
 
-// write your code here
+const statusbar = document.getElementById("num");
+const resetBtn = document.getElementById("reset");
+const img = document.getElementById("img");
+
+function fetchIssue(nums){
+    let url = XKCD + nums
+    console.log(`request to ${url}`)
+    let promesse = fetch(url);
+    promesse.then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+        let num = json.num;
+        console.log(`num: ${num}`);
+        statusbar.innerText = `num: ${num}`;
+        let image = json.img;
+        img.src = image;
+
+    })
+}
+
+const buttonAction = () => {
+    fetchIssue("latest");
+}
+
+resetBtn.addEventListener("click", buttonAction);
